088aaa43cca3b6254a0a3fd52ede2a63dcfb189d
hakave <veeti.hakala@jamk.fi>
2023-10-28 18:12:30 +0300

    product model removed

--- front/app/models/product.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class ProductModel extends Model {
-  @attr('string') name;
-  @attr('string') description;
-  @attr('string') price;
-}
